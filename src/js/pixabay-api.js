c8ffde77d5c0b754175efd855464e72c9976c704
letonzt <letonzt@gmail.com>
2025-03-28 20:25:13 +0100

    edit file pixabay-api.js>correct my  API

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const fetchImages = async (searchQuery, page = 1) => {
-  const API_KEY = ' 49580419-995b76e467c7e33d07d0013ac';
+  const API_KEY = '49580419-995b76e467c7e33d07d0013ac';
   const BASE_URL = 'https://pixabay.com/api/';
 
   try {
     const response = await axios.get(BASE_URL, {
       params: {
-        key: ' 49580419-995b76e467c7e33d07d0013ac',
+        key: '49580419-995b76e467c7e33d07d0013ac',
         q: searchQuery,
         image_type: 'photo',
         orientation: 'horizontal',
